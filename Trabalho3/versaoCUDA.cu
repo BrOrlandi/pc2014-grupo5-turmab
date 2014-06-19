d133b214200f42e2e07e21525d86f7b0e6e527e9
Rafael Ribaldo <ribaldorafael@gmail.com>
2014-06-19 16:46:59 -0300

    trabalho3 - versão sequencial e paralela

---
+++ Trabalho3/versaoCUDA.cu
@@ -0,0 +1,137 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Tamanho do filtro, como definido na especificação: 5x5.
+#define TAM_FILTRO 5
+
+#define GRID 1
+#define BLOCK 1024
+#define TILE_WIDTH 28
+#define BLOCK_WIDTH (TILE_WIDTH + (TAM_FILTRO - 1))
+
+// Kernel de convolução
+__global__ void convolucao(int *output, int *input, int width, int height){
+
+        // Linha atual
+        int idxY = blockIdx.y * TILE_WIDTH + threadIdx.y;
+
+        // Coluna atual
+        int idxX = blockIdx.x * TILE_WIDTH + threadIdx.x;
+
+        __shared__ int sharedMemory[BLOCK_WIDTH][BLOCK_WIDTH];
+
+        int offset = TAM_FILTRO / 2;
+        int sharedX = threadIdx.x, sharedY = threadIdx.y;
+        int row_i = idxY - offset;
+        int col_i = idxX - offset;
+
+        for(int channelIndex = 0; channelIndex < 3; channelIndex++){
+                int index = (row_i * width + col_i) * 3 + channelIndex;
+
+                if((row_i >= 0) && (row_i < height) &&
+                        (col_i >= 0) && (col_i < width))
+                                sharedMemory[sharedY][sharedX] = input[index];
+
+                else
+                        sharedMemory[sharedY][sharedX] = 0;
+
+                __syncthreads();
+
+                int total = 0;
+
+                // Verifica se valores de linha e coluna pertencem �|  matriz 
+                if(sharedY < TILE_WIDTH && sharedX < TILE_WIDTH){
+                        for(int i = 0; i < TAM_FILTRO; i++)
+                                for(int j = 0; j < TAM_FILTRO; j++)
+                                        total += sharedMemory[sharedY+i][sharedX+j];
+                        // Média dos pixels
+                        if(idxY < height && idxX < width)
+                                output[(idxY * width + idxX) * 3 + channelIndex] = total / (TAM_FILTRO * TAM_FILTRO);
+
+                        __syncthreads();
+                }
+        }
+}
+
+int main(int argc, char *argv[]){
+
+        int totalX, totalY, N;
+
+        FILE *arquivo;
+        arquivo = fopen(argv[1], "r");
+
+        if(arquivo == NULL){
+                printf("Arquivo não pode ser encontrado!\n");
+                exit(1);
+        }
+
+        // P3
+        fseek(arquivo, 2, SEEK_SET);
+
+        // tamanho da imagem
+        fscanf(arquivo, "%d %d", &totalX, &totalY);
+        N = totalX * totalY * 3;
+
+        //declara matriz de entrada e final
+        int *input, *outputFinal;
+
+        input = (int *) malloc(sizeof(int) * N);
+        outputFinal = (int *) malloc(sizeof(int) * N);
+
+        //declara matrizes de entrada e saída (CUDA)
+        int *dev_input, *dev_output;
+
+        cudaMalloc((void**) &dev_input, N * sizeof(int));
+        cudaMalloc((void**) &dev_output, N * sizeof(int));
+
+        // 255
+        fseek(arquivo, 4, SEEK_CUR);
+
+        // carrega matriz
+        //printf("%d %d\n", totalX, totalY);
+        for(int i = 0; i < N; i++){
+                fscanf(arquivo, "%d", &input[i]);
+                //printf("input[%d]: %d\n", i, input[i]);
+        }
+
+        cudaMemcpy(dev_input, input, N * sizeof(int), cudaMemcpyHostToDevice);
+
+        dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH);
+        dim3 dimGrid((totalX-1) / TILE_WIDTH + 1, (totalY-1) / TILE_WIDTH + 1);
+
+        convolucao<<<dimGrid, dimBlock>>>(dev_output, dev_input, totalX, totalY);
+
+        cudaMemcpy(outputFinal, dev_output, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+        //Imprimi matriz
+//      for(int i = 0; i < N; i++)
+//              printf("%d\n", outputFinal[i]);
+
+        //Salva no arquivo de saida
+        char str_final[100];
+
+        strcpy(str_final, "out_cuda_");
+        strcat(str_final, argv[1]);
+
+        arquivo = fopen(str_final, "w");
+
+        if(arquivo != NULL){
+                fprintf(arquivo, "P3\n%d %d\n255\n", totalX, totalY);
+
+                for(int i = 0; i < N; i++)
+                        fprintf(arquivo, "%d\n", outputFinal[i]);
+
+                fclose(arquivo);
+        }
+
+        else
+                printf("Arquivo não pode ser criado!\n");
+
+        free(input);
+        free(outputFinal);
+        cudaFree(dev_input);
+        cudaFree(dev_output);
+
+        return 0;
+}
---
+++ Trabalho3/versaoSequencial.c
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Le a entrada e divide nas 3 matrizes
+
+void carregaMatriz(FILE *arquivo, int totalX, int totalY, int **matrizR,int **matrizG,int **matrizB){
+
+    int i,j;
+
+    for(i=0;i<totalX;i++){
+
+        for(j=0;j<totalY;j++){
+
+            fscanf(arquivo, "%d %d %d ",&matrizR[i][j],&matrizG[i][j],&matrizB[i][j]);
+
+        }
+
+    }
+
+}
+
+
+
+//Imprimi matriz na tela
+
+void imprimiMatriz(int totalX, int totalY, int **matriz){
+
+    int i,j;
+
+    for(i=0;i<totalX;i++){
+
+        for(j=0;j<totalY;j++){
+
+            printf("%d ",matriz[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+}
+
+
+
+//Salva 3 matrizes no arquivo .ppm seguindo a formatacao necessaria
+
+void salvaMatriz(FILE *arquivo, int totalX, int totalY, int **matrizR, int **matrizG, int **matrizB){
+
+    int i,j;
+
+//    fprintf(arquivo, "P3\n");
+
+//    fprintf(arquivo, "# out.ppm\n");
+
+//    fprintf(arquivo, "%d %d\n", totalX, totalY);
+
+//    fprintf(arquivo, "255\n");
+
+//    if(arquivo != NULL){
+
+        for(i=0;i<totalX;i++){
+
+            for(j=0;j<totalY;j++){
+
+                fprintf(arquivo, "%d\n%d\n%d\n", matrizR[i][j], matrizG[i][j], matrizB[i][j]);
+
+            }
+
+        }
+
+        fclose(arquivo);
+
+//    }else{
+
+//        printf("Arquivo nao pode ser criado!\n");
+
+//    }
+
+}
+
+
+
+//Realiza a convolucao da matriz de entrada e salva o resultado na matriz de saida
+
+void convolucao(int totalX, int totalY, int **matrizEntrada, int **matrizSaida){
+
+    int i,j,xi,yj;
+
+    int total;
+
+    for(i=0;i<totalX;i++){
+
+        for(j=0;j<totalY;j++){
+
+            total = 0;
+
+            for(xi=i-2;xi<=i+2;xi++){
+
+                for(yj=j-2;yj<=j+2;yj++){
+
+                    //Verifica se coordenadas sao validas e assume valor zero caso nao sejam
+
+                    if((xi < 0) || (xi >= totalX) || (yj < 0) || ((yj >= totalY))){
+
+                        total += 0;
+
+                    }else{
+
+						total += matrizEntrada[xi][yj];
+
+                    }
+
+                }
+
+            }
+
+            total = total/25;
+
+            matrizSaida[i][j] = total;
+
+        }
+
+    }
+
+}
+
+
+
+int main(int argc, char *argv[]){
+        FILE *arquivo = fopen(argv[1], "r");
+    int totalX,totalY, i;
+
+
+        if(arquivo == NULL){
+                printf("Arquivo não pode ser encontrado!\n");
+                exit(1);
+        }
+
+        // P3
+        fseek(arquivo, 2, SEEK_SET);
+
+
+        // tamanho da image
+        fscanf(arquivo, "%d %d", &totalX, &totalY);
+
+        // 255
+        fseek(arquivo, 4, SEEK_CUR);
+
+
+    //declara matrizes de entrada
+
+    int **matrizR, **matrizG, **matrizB;
+
+        matrizR = (int **) malloc(sizeof(int *) * totalX);
+        matrizG = (int **) malloc(sizeof(int *) * totalX);
+		matrizB = (int **) malloc(sizeof(int *) * totalX);
+
+        for(i = 0; i < totalX; i++){
+                matrizR[i] = (int *) malloc(sizeof(int) * totalY);
+                matrizG[i] = (int *) malloc(sizeof(int) * totalY);
+                matrizB[i] = (int *) malloc(sizeof(int) * totalY);
+        }
+
+    //declara matrizes de saida
+
+    int **matrizSaidaR, **matrizSaidaG, **matrizSaidaB;
+
+        matrizSaidaR = (int **) malloc(sizeof(int *) * totalX);
+        matrizSaidaG = (int **) malloc(sizeof(int *) * totalX);
+        matrizSaidaB = (int **) malloc(sizeof(int *) * totalX);
+
+        for(i = 0; i < totalX; i++){
+                matrizSaidaR[i] = (int *) malloc(sizeof(int) * totalY);
+                matrizSaidaG[i] = (int *) malloc(sizeof(int) * totalY);
+                matrizSaidaB[i] = (int *) malloc(sizeof(int) * totalY);
+        }
+    //carrega todas as matrizes
+
+    carregaMatriz(arquivo, totalX, totalY, matrizR, matrizG, matrizB);
+
+        fclose(arquivo);
+
+
+    //faz convolucao em cada uma das 3 matrizes
+
+    convolucao(totalX, totalY, matrizR, matrizSaidaR);
+
+    convolucao(totalX, totalY, matrizG, matrizSaidaG);
+
+    convolucao(totalX, totalY, matrizB, matrizSaidaB);
+
+	char str_final[100];
+
+        strcpy(str_final, "out_seq_");
+        strcat(str_final, argv[1]);
+
+        arquivo = fopen(str_final, "w");
+
+        if(arquivo == NULL){
+                printf("Arquivo não pode ser encontrado!\n");
+                exit(1);
+        }
+
+        fprintf(arquivo, "P3\n%d %d\n255\n", totalX, totalY);
+
+
+    //Salva no arquivo de saida
+
+    salvaMatriz(arquivo, totalX, totalY, matrizSaidaR, matrizSaidaG, matrizSaidaB);
+
+
+
+    //Imprimi cada uma das matrizes
+
+//    imprimiMatriz(totalX, totalY, matrizSaidaR);
+
+ //   printf("-----------------------\n");
+
+  //  imprimiMatriz(totalX, totalY, matrizSaidaG);
+
+  //  printf("-----------------------\n");
+
+  //  imprimiMatriz(totalX, totalY, matrizSaidaB);
+
+        for(i = 0; i < totalX; i++){
+                free(matrizR[i]);
+                free(matrizG[i]);
+                free(matrizB[i]);
+                free(matrizSaidaR[i]);
+                free(matrizSaidaG[i]);
+                free(matrizSaidaB[i]);
+        }
+
+        free(matrizR);
+        free(matrizG);
+        free(matrizB);
+        free(matrizSaidaR);
+        free(matrizSaidaG);
+        free(matrizSaidaB);
+
+    return 0;
+
+}
+
